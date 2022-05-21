df1aee9f0e96f3c9b60c22a6dba6ea1dba28b17d
Samin Israr Ravi <saminisrar1@gmail.com>
2022-05-22 01:27:29 +0600

    accordion and subscribe form added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,6 +6,8 @@ import {
   faStar,
   faEarthAmerica,
 } from "@fortawesome/free-solid-svg-icons";
+import FAQ from "../FAQ/FAQ";
+import Subscribe from "../Subscribe/Subscribe";
 
 const Home = () => {
   return (
@@ -23,6 +25,17 @@ const Home = () => {
           number={100}
         ></Summary>
         <Summary icon={faStar} header="+ Feedback" number={134}></Summary>
+      </div>
+      <div className="my-5 container">
+        <h1 className="text-center text-muted mb-4 ">
+          Frequently Asked Questions
+        </h1>
+        <FAQ></FAQ>
+      </div>
+
+      <div className="my-5 container">
+        <h1 className="text-center text-danger mb-4">Get In Touch With Us!</h1>
+        <Subscribe></Subscribe>
       </div>
     </div>
   );
---
+++ src/components/Subscribe/Subscribe.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Form } from 'react-bootstrap';
+
+const Subscribe = () => {
+    return (
+      <div className="d-flex justify-content-around align-items-center">
+        <div>
+          <img
+            className="w-75"
+            src="https://img.freepik.com/free-vector/team-engineers-workers-controlling-oil-gas-pipeline_74855-20378.jpg?t=st=1653160607~exp=1653161207~hmac=515ca605ab42597efbc49ce4956076687d562ab6bd1a4425d726e100121a1f3a&w=740"
+            alt="subscribe"
+          />
+        </div>
+        <div className="w-50">
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="name@example.com" />
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Label>Your Feedback</Form.Label>
+              <Form.Control as="textarea" rows={3} />
+              <div>
+                  <button className="btn btn-success d-block mx-auto my-4">Submit</button>
+              </div>
+            </Form.Group>
+          </Form>
+        </div>
+      </div>
+    );
+};
+
+export default Subscribe;
