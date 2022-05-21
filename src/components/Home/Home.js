5c6da29b00b81b5205eba9e8cdfbd7988aff7415
Samin Israr Ravi <saminisrar1@gmail.com>
2022-05-22 01:04:57 +0600

    business summary added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React from "react";
+import Banner from "../Banner/Banner";
+import Summary from "../Summary/Summary";
+import {
+  faPeopleGroup,
+  faStar,
+  faEarthAmerica,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Home = () => {
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    );
+  return (
+    <div>
+      <Banner></Banner>
+      <div className="d-flex justify-content-evenly my-3 bg-success py-5">
+        <Summary
+          icon={faEarthAmerica}
+          header="+ Countries"
+          number={47}
+        ></Summary>
+        <Summary
+          icon={faPeopleGroup}
+          header="+ Happy Clients"
+          number={100}
+        ></Summary>
+        <Summary icon={faStar} header="+ Feedback" number={134}></Summary>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/components/Summary/Summary.js
+++ src/components/Summary/Summary.js
@@ -1,10 +1,21 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import "./Summary.css"
 import React from 'react';
+import CountUp from "react-countup";
 
-const Summary = () => {
+const Summary = ({icon, number, header}) => {
+
     return (
+      <div >
         <div>
-            <h1>Summary</h1>
+          <div>
+            <FontAwesomeIcon className="icon d-block mx-auto text-white" icon={icon} />
+          </div>
+          <div className="mt-3">
+            <CountUp className="number text-white " duration="3" end={number} /> <span className="summary-header text-white">{header}</span>
+          </div>
         </div>
+      </div>
     );
 };
 
